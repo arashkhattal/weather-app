ce7910889ea28a8047373b1793a4d42ae4d51c66
arashkhattal <arashkhattal@gmail.com>
2022-09-26 19:04:48 +0530

    initial commit test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,5 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Lets make "React-Dashboard"
-        </a>
-      </header>
-    </div>
-  );
+  return <div>hello</div>;
 }
 
 export default App;
